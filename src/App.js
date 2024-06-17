c4d3d92211dca0f3d0c4089ef2e58175d3c06fd5
Kearabetswejabane <keahjabane@gmail.com>
2024-06-17 11:21:29 +0200

    Created a footer,added Github repository Url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Weather App</header>
+      <header className="App-header">
+        <h1>Weather App </h1>
+        <footer>
+          This project was coded by Kearabetswe Jabane and is {""}
+          <a
+            href="https://github.com/Kearabetswejabane/react-weather-app"
+            target="blank"
+          >
+            Open Sourced
+          </a>
+        </footer>
+      </header>
     </div>
   );
 }
