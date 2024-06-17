b6425e6fc8443f227ffa8fc01cbca076cfdaa5a9
Kearabetswejabane <keahjabane@gmail.com>
2024-06-17 11:41:23 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ function App() {
       <header className="App-header">
         <h1>Weather App </h1>
         <footer>
-          This project was coded by Kearabetswe Jabane and is {""}
+          This project was coded by{" "}
+          <a href="https://github.com/Kearabetswejabane" target="blank">
+            Kearabetswe Jabane
+          </a>{" "}
+          and is {""}
           <a
             href="https://github.com/Kearabetswejabane/react-weather-app"
             target="blank"
